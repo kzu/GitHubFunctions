7a701d907dcd08ee08d50bbd14a33a3b03416d25
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-19 16:56:42 -0300

    Render request headers by default

--- Auth0Functions/Function.cs
+++ Auth0Functions/Function.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Azure.Functions.Worker;
+using System.Security.Claims;
 
 namespace Auth0Functions;
 
@@ -11,6 +12,8 @@ public class Function(ILogger<Function> logger)
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        return new OkObjectResult($"Welcome to Azure Functions!" + string.Join(Environment.NewLine,
+            req.Headers.Select(x => $"{x.Key} = {x.Value}")));
     }
 }
