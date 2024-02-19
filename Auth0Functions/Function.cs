011318918fbe34536735c7ba47ee8f1e83f0cf49
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-19 10:35:10 -0300

    Switch to .NET8 isolated mode

--- Auth0Functions/Function.cs
+++ Auth0Functions/Function.cs
@@ -1,19 +1,16 @@
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
 
 namespace Auth0Functions;
 
-public static class Function
+public class Function(ILogger<Function> logger)
 {
-    [FunctionName("echo")]
-    public static IActionResult Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
+    [Function("echo")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
-        return new OkObjectResult("echo");
+        logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
