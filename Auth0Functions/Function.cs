8815fce37524cd3b993d756fa0586e37e5caa0ab
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-19 23:17:31 -0300

    Get claims from HttpRequest too
    This should be populated automatically by [Authorize]

--- Auth0Functions/Function.cs
+++ Auth0Functions/Function.cs
@@ -16,19 +16,20 @@ public class Function(ILogger<Function> logger)
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var feature = context.Features.Get<PrincipalFeature>();
-        if (feature?.Principal.Identity?.IsAuthenticated != true)
+        var principal = feature?.Principal ?? req.HttpContext.User;
+
+        if (principal.Identity?.IsAuthenticated != true)
         {
             return new OkObjectResult($"Not authenticated :(" +
             "\r\n\r\n-- Headers --\r\n" +
             string.Join(Environment.NewLine, req.Headers.Select(x => $"{x.Key} = {x.Value}")));
         }
 
-        var user = feature.Principal;
-
-        return new OkObjectResult($"Welcome to Azure Functions!" + 
-            "\r\n\r\n-- Claims --\r\n" + 
-            string.Join(Environment.NewLine, user.Claims.Select(x => $"{x.Type} = {x.Value}")) +
+        return new OkObjectResult($"Welcome to Azure Functions!" +
+            "\r\n\r\n-- Claims --\r\n" +
+            string.Join(Environment.NewLine, principal.Claims.Select(x => $"{x.Type} = {x.Value}")) +
             "\r\n\r\n-- Headers --\r\n" +
             string.Join(Environment.NewLine, req.Headers.Select(x => $"{x.Key} = {x.Value}")));
+
     }
 }
