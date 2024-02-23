31c30b929d74a50f3cb1edbd6eee54203cc40d5a
Daniel Cazzulino <daniel@cazzulino.com>
2024-02-23 16:12:59 -0300

    Import generic features from catbag

--- GitHubFunctions/Microsoft/Azure/Functions/AppServiceAuthenticationExtensions.cs
+++ GitHubFunctions/Microsoft/Azure/Functions/AppServiceAuthenticationExtensions.cs
@@ -1,15 +1,35 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 using Azure.Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Middleware;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Extensions.Hosting;
 
+/// <summary>
+/// Implements app service authentication for Azure Functions, by automatically 
+/// populating a <see cref="ClaimsPrincipal"/> from the request headers 
+/// into the <see cref="FunctionContext.Features"/>.
+/// </summary>
+/// <remarks>
+/// The populated <see cref="ClaimsPrincipal"/> can be retrieved from the function 
+/// context using <see cref="FunctionContext.Features"/>. For example:
+/// <code>
+/// ClaimsPrincipal? principal = functionContext.Features.Get&lt;ClaimsPrincipal&gt;();
+/// </code>
+/// See https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-user-identities.
+/// </remarks>
 public static partial class AppServiceAuthenticationExtensions
 {
+    /// <summary>
+    /// Parses existing app service authentication headers and sets them as 
+    /// <see cref="FunctionContext.Features"/>.Set(<see cref="ClaimsPrincipal"/>) and 
+    /// <see cref="FunctionContext.Features"/>.Set(<see cref="AccessToken"/>) if present.
+    /// </summary>
     public static IFunctionsWorkerApplicationBuilder UseAppServiceAuthentication(this IFunctionsWorkerApplicationBuilder builder)
         => builder.UseMiddleware<ClientPrincipalMiddleware>();
 
@@ -21,13 +41,14 @@ public static partial class AppServiceAuthenticationExtensions
         {
             if (context.Features.Get<ClaimsPrincipal>() is not null)
             {
+                // Already set by some other component.
                 await next(context);
                 return;
             }
 
             var req = await context.GetHttpRequestDataAsync();
             if (req is not null &&
-                req.Headers.ToDictionary(x => x.Key, x => string.Join(',', x.Value), StringComparer.OrdinalIgnoreCase) is var headers &&
+                req.Headers.ToDictionary(x => x.Key, x => string.Join(",", x.Value), StringComparer.OrdinalIgnoreCase) is var headers &&
                 headers.TryGetValue("x-ms-client-principal", out var msclient) &&
                 Convert.FromBase64String(msclient) is var decoded &&
                 Encoding.UTF8.GetString(decoded) is var json &&
@@ -40,7 +61,7 @@ public static partial class AppServiceAuthenticationExtensions
                 context.Features.Set(principal);
 
                 if (headers.TryGetValue($"x-ms-token-{cp.auth_typ}-access-token", out var token))
-                    context.Features.Set(new AccessToken(token, DateTimeOffset.MinValue));
+                    context.Features.Set(new AccessToken(token, DateTimeOffset.MaxValue));
             }
 
             await next(context);
