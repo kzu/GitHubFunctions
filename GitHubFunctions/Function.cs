5f256c582812f3efdd66552d7bf579d02646e235
Daniel Cazzulino <daniel@cazzulino.com>
2024-04-05 12:13:15 -0300

    Add client_id claim to output
    This allows the client to determine what's the GH OAuth app performing the authentication, i.e. to de-authorize as needed.

--- GitHubFunctions/Function.cs
+++ GitHubFunctions/Function.cs
@@ -16,18 +16,20 @@ public class Function(ILogger<Function> logger, IConfiguration configuration, IH
     [Function("me")]
     public async Task<IActionResult> EchoAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
     {
+        var clientId = configuration["WEBSITE_AUTH_GITHUB_CLIENT_ID"];
+        if (string.IsNullOrEmpty(clientId))
+        {
+            logger.LogError("Ensure GitHub identity provider is configured for the functions app.");
+            return new StatusCodeResult(500);
+        }
+
         if (ClaimsPrincipal.Current is not { Identity.IsAuthenticated: true } principal)
         {
             // Implement manual auto-redirect to GitHub, since we cannot turn it on in the portal
             // or the token-based principal population won't work.
             // Never redirect requests for JWT, as they are likely from a CLI or other non-browser client.
-            if (!req.Headers.Accept.Contains("application/json") &&
-                configuration["WEBSITE_AUTH_GITHUB_CLIENT_ID"] is { Length: > 0 } clientId)
-            {
+            if (!req.Headers.Accept.Contains("application/json"))
                 return new RedirectResult($"https://github.com/login/oauth/authorize?client_id={clientId}&scope=read:user%20read:org%20user:email&redirect_uri=https://{req.Headers["Host"]}/.auth/login/github/callback&state=redir=/me");
-            }
-
-            logger.LogWarning("Ensure WEBSITE_AUTH_GITHUB_CLIENT_ID configuration is present.");
 
             // Otherwise, just 401
             return new UnauthorizedResult();
@@ -42,12 +44,17 @@ public class Function(ILogger<Function> logger, IConfiguration configuration, IH
         var body = await response.Content.ReadFromJsonAsync<JsonObject>();
         body?.Add("emails", emails);
 
+        var claims = principal.Claims.GroupBy(x => x.Type)
+                .Select(g => new { g.Key, Value = (object)(g.Count() == 1 ? g.First().Value : g.Select(x => x.Value).ToArray()) })
+                .ToDictionary(x => x.Key, x => x.Value);
+
+        // Allows the client to authenticate directly with the OAuth app if needed too.
+        claims["client_id"] = clientId; 
+
         return new JsonResult(new
         {
             body,
-            claims = principal.Claims.GroupBy(x => x.Type)
-                .Select(g => new { g.Key, Value = (object)(g.Count() == 1 ? g.First().Value : g.Select(x => x.Value).ToArray()) })
-                .ToDictionary(x => x.Key, x => x.Value),
+            claims,
             request = req.Headers.ToDictionary(x => x.Key, x => x.Value.ToString().Trim('"')),
             response = response.Headers.ToDictionary(x => x.Key, x => string.Join(',', x.Value)),
         })
